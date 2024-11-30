ec7e2f2c8bdedc68212ceca513fd35ec2e0481f8
chiragsingh1 <imchirag.2717@gmail.com>
2024-12-01 00:37:18 +0530

    /hooks/catch endpoint implemented, transactional outbox

---
+++ hooks/src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const db = new PrismaClient();
+
+app.use(express.json());
+
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+
+    // store in DB a new trigger
+    await db.$transaction(async (tx) => {
+        const run = await db.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: req.body,
+            },
+        });
+
+        await db.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id,
+            },
+        });
+    });
+
+    // push it into a queue redis/kafka
+
+    res.json({
+        message: "Webhook received",
+    });
+});
+
+app.listen(3000);
